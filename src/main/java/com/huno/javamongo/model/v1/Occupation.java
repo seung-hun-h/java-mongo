b62516fedb6d62a39e6031429416c25e961dcd89
seunghun.ham <seunghun.ham@nts-corp.com>
2025-02-18 13:48:41 +0900

    v1

--- src/main/java/com/huno/javamongo/model/v1/Occupation.java
+++ src/main/java/com/huno/javamongo/model/v1/Occupation.java
@@ -3,14 +3,4 @@ package com.huno.javamongo.model.v1;
 import java.time.LocalDateTime;
 
 public record Occupation(String name, String company, int salary, LocalDateTime joinTime) {
-
-	@Override
-	public String toString() {
-		return "Occupation{" +
-			"name='" + name + '\'' +
-			", company='" + company + '\'' +
-			", salary=" + salary +
-			", joinTime=" + joinTime +
-			'}';
-	}
 }
--- src/main/java/com/huno/javamongo/model/v1/Person.java
+++ src/main/java/com/huno/javamongo/model/v1/Person.java
@@ -3,13 +3,4 @@ package com.huno.javamongo.model.v1;
 import java.util.List;
 
 public record Person(String name, int age, List<Occupation> occupations) {
-
-	@Override
-	public String toString() {
-		return "Person{" +
-			"name='" + name + '\'' +
-			", age=" + age +
-			", occupations=" + occupations +
-			'}';
-	}
 }
